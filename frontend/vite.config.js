f2fe1959d319b614e645f775e5037b1bd64dc031
MSuster <sustermauricio@gmail.com>
2025-09-22 11:03:04 -0300

    mais um commit falho quer ver

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import netlify from '@netlify/vite-plugin';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    netlify()
-  ]
+  plugins: [react()],
+  build: {
+    outDir: 'dist', // certifique-se de apontar para o publish do Netlify
+  },
 });
